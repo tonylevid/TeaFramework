9df519f26a403155da0b21d6b55d6db3a749b77f
tonylevid <tonylevid@gmail.com>
2014-06-30 20:36:41 +0800

    relations as join

--- demo/protected/module/test/model/TestModel.php
+++ demo/protected/module/test/model/TestModel.php
@@ -9,13 +9,14 @@ class TestModel extends TeaModel {
     public function criterias() {
         return array(
             'test_detail' => array(
-                'join' => array('left:test_detail->TD' => array('TD.parent_id' => 'A.id'))
+                'join' => array('left:test_detail->TD' => array('TD.parent_id' => 'A.id')),
+                'where' => array('A.id:lt' => 5)
             )
         );
     }
 
     public function all() {
-        return $this->withCriteria('test_detail')->findAll(array('A.id:gt' => 0), array(
+        return $this->withCriteria('test_detail')->findAllByCondition(array('A.id:gt' => 0), array(
             new TeaDbExpr('A.id + ? AS a_id_plus_two', array(2)),
             'A.id->a_id',
             'A.name->a_name',
