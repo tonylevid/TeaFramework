31a2a3c62f6d65fdf2421ead8c2a509b02a71807
tonylevid <taolei@taoleideMac-mini.local>
2014-08-22 11:50:43 +0800

    connect multi-databases bug fixed

--- tea/TeaBase.php
+++ tea/TeaBase.php
@@ -313,11 +313,9 @@ class TeaBase {
         }
         if (isset(self::$_connectionTypeMap[$driverType])) {
             $connClass = self::$_connectionTypeMap[$driverType];
-            if (!self::$_connection instanceof $connClass) {
-                self::$_connection = new $connClass($connInfo);
-                if ($autoConnect) {
-                    self::$_connection->connect();
-                }
+            self::$_connection = new $connClass($connInfo);
+            if ($autoConnect) {
+                self::$_connection->connect();
             }
         } else {
             throw new TeaDbException("TeaBase could not determine the driver type, check your dsn '{$dsn}'.");
