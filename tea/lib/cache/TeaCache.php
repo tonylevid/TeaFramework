1be88ab21601820f00b63a623c1a8168f2b203cd
tonylevid <tonylevid@gmail.com>
2014-12-12 15:16:12 +0800

    TeaCache文档

--- tea/lib/cache/TeaCache.php
+++ tea/lib/cache/TeaCache.php
@@ -5,16 +5,33 @@ class TeaCache implements TeaICache {
     const CACHE_TYPE_FILE = 'TeaCacheFile';
     
     protected $_cacheInstance;
-
+    
+    /**
+     * 构造函数。
+     * @param string $constCacheType CACHE_TYPE_*的类常量，默认为CACHE_TYPE_FILE。
+     * @param array $cacheInstCntrArgs CACHE_TYPE_*的类常量对应类的构造函数参数。
+     */
     public function __construct($constCacheType = self::CACHE_TYPE_FILE, $cacheInstCntrArgs = array()) {
         $rfc = new ReflectionClass($constCacheType);
         $this->_cacheInstance = $rfc->newInstanceArgs($cacheInstCntrArgs);
     }
     
+    /**
+     * 缓存数据。
+     * @param string $key 缓存数据名，圆点标记法别名。
+     * @param mixed $val 缓存数据。
+     * @param int $expire 过期时间，单位为秒。
+     * @return int 返回写入文件的字节数，如果失败则返回false。
+     */
     public function cache($key, $val, $expire = 0) {
         return $this->_cacheInstance->cache($key, $val, $expire);
     }
     
+    /**
+     * 获取缓存数据。
+     * @param string $key 缓存数据名，圆点标记法别名。
+     * @return mixed 返回缓存数据，如果失败则返回false。
+     */
     public function getCache($key) {
         return $this->_cacheInstance->getCache($key);
     }
