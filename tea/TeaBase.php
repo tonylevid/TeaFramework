0bedaaecf4a60ce63789c727a9475689331eb551
tonylevid <tonylevid@gmail.com>
2015-01-06 11:23:36 +0800

    修复异常处理bug

--- tea/TeaBase.php
+++ tea/TeaBase.php
@@ -84,12 +84,16 @@ class TeaBase {
      * @param array $routeArgs 手动设置的路由参数。
      */
     public static function run($config = array(), $routeArgs = array()) {
-        self::init($config);
-        self::getRouter()->route($routeArgs);
-        defined('APP_END_TIME') or define('APP_END_TIME', microtime(true));
-        defined('APP_END_MEM') or define('APP_END_MEM', memory_get_usage());
-        defined('APP_USED_TIME') or define('APP_USED_TIME', APP_END_TIME - APP_BEGIN_TIME);
-        defined('APP_USED_MEM') or define('APP_USED_MEM', APP_END_MEM - APP_BEGIN_MEM);
+        try {
+            self::init($config);
+            self::getRouter()->route($routeArgs);
+            defined('APP_END_TIME') or define('APP_END_TIME', microtime(true));
+            defined('APP_END_MEM') or define('APP_END_MEM', memory_get_usage());
+            defined('APP_USED_TIME') or define('APP_USED_TIME', APP_END_TIME - APP_BEGIN_TIME);
+            defined('APP_USED_MEM') or define('APP_USED_MEM', APP_END_MEM - APP_BEGIN_MEM);
+        } catch (Exception $exception) {
+            self::exceptionHandler($exception);
+        }
     }
 
     /**
